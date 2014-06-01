13f7a9cd38a6ac00647c5005cb0db24c1322def3
Daniel Buckmaster <dan.buckmaster@gmail.com>
2014-06-01 14:21:33 +1000

    Improvements.

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var map = knowledgeMap.create({
     }, {
       id: 'httpobject',
       name: 'Talk to a server',
-      dependencies: ['basic-ts'],
+      dependencies: ['objects'],
       content: {
         description: 'Use the `HTTPObject` class to interact with a remote server.'
       }
@@ -40,6 +40,54 @@ var map = knowledgeMap.create({
       dependencies: ['objects', 'keybinds'],
       content: {
         description: 'Fly around the level as a floating eyeball!'
+      }
+    }, {
+      id: 'basic-hud',
+      name: 'Edit the HUD',
+      dependencies: ['objects'],
+      content: {
+        description: 'Add a simple heads-up display on top of the game display'
+      }
+    }, {
+      id: 'basic-gui',
+      name: 'Make a main menu',
+      dependencies: ['basic-hud'],
+      content: {
+        description: 'Start your game in a main menu, not a 3D world view.'
+      }
+    }, {
+      id: 'basic-rift',
+      name: 'Oculus Rift basics',
+      dependencies: ['compiling', 'basic-ts'],
+      content: {
+        description: 'Set up integeration with the Oculus Rift.'
+      }
+    }, {
+      id: 'compiling',
+      name: 'Compile the engine',
+      content: {
+        description: 'Get set up to compile Torque from its source code.'
+      }
+    }, {
+      id: 'lan-networking',
+      name: 'Host a LAN game',
+      dependencies: ['networking'],
+      content: {
+        description: 'Make your game visible to other clients on the same LAN.'
+      }
+    }, {
+      id: 'networking',
+      name: 'Networking',
+      dependencies: [],
+      content: {
+        description: 'Add networking to your game!'
+      }
+    }, {
+      id: 'startup',
+      name: 'Understanding startup',
+      dependencies: ['basic-ts'],
+      content: {
+        description: 'Get to know how the engine startup sequence works and why `main.cs` looks the way it does.'
       }
     }]
   },
